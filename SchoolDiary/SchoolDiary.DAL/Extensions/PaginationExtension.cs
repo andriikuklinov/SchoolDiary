d4188e8f3b33ca5ef0b7180b023f93096b645aa3
akuklinov <akuklinov@outlook.com>
2024-08-21 16:36:54 +0300

    Add pagination extension method.

---
+++ SchoolDiary/SchoolDiary.DAL/Extensions/PaginationExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.DAL.Extensions
+{
+    public static class PaginationExtension
+    {
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> query, int? page, int? pageSize)
+        {
+            if (page == null || pageSize == null)
+            {
+                return query;
+            }
+            else
+            {
+                if(pageSize <= 0)
+                    throw new ArgumentOutOfRangeException(nameof(pageSize), pageSize, "Invalid pageSize value.");
+                if (page != 0)
+                    query = query.Skip((int)(page * pageSize));
+                return query.Take((int)pageSize);
+            }
+
+        }
+    }
+}
