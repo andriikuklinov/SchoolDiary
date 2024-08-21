b19932f08ee9890f2700a841daf235a459fb05a8
akuklinov <akuklinov@outlook.com>
2024-08-21 15:05:22 +0300

    Implementing GenericRepository.

--- SchoolDiary/SchoolDiary.DAL/Repositories/IRepositories/IGenericRepository.cs
+++ SchoolDiary/SchoolDiary.DAL/Repositories/IRepositories/IGenericRepository.cs
@@ -9,12 +9,13 @@ namespace SchoolDiary.DAL.Repositories.IRepositories
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> GetSingleASync(Expression<Func<T, bool>> filter = null, CancellationToken cancellationToken = default);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> filter = null, CancellationToken cancellationToken = default);
         IQueryable<T> Get(Expression<Func<T, bool>> filter, CancellationToken cancellationToken = default);
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task<T> DeleteAsync(T entity);
         Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task<IEnumerable<T>> UpdateRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> DeleteRangeAsync(IEnumerable<T> entities);
     }
 }
