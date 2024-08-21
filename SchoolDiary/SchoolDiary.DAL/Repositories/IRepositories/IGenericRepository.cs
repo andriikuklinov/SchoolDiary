d874336398942dd414f976d39bce60e3a9a3aad5
akuklinov <akuklinov@outlook.com>
2024-08-21 14:44:23 +0300

    Create IGenericRepository contract.

---
+++ SchoolDiary/SchoolDiary.DAL/Repositories/IRepositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.DAL.Repositories.IRepositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetSingleASync(Expression<Func<T, bool>> filter = null, CancellationToken cancellationToken = default);
+        IQueryable<T> Get(Expression<Func<T, bool>> filter, CancellationToken cancellationToken = default);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> UpdateRangeAsync(IEnumerable<T> entities);
+    }
+}
