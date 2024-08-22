c5e9ce3bae25839914fa752efceec4719bbde2fd
akuklinov <akuklinov@outlook.com>
2024-08-22 11:26:02 +0300

    Create DateOnly/DateTime converter.

---
+++ SchoolDiary/SchoolDiary.BLL/Converters/DateOnlyToDateTimeConverter.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.BLL.Converters
+{
+    internal class DateOnlyToDateTimeConverter : IValueConverter<DateOnly, DateTime>
+    {
+        public DateTime Convert(DateOnly sourceMember, ResolutionContext context)
+        {
+            return sourceMember.ToDateTime(TimeOnly.Parse("00:00 AM"));
+        }
+    }
+}
