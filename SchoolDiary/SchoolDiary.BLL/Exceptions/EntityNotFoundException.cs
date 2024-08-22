272c7d3147b39c6cb2478be38862bd76c3cd293a
akuklinov <akuklinov@outlook.com>
2024-08-22 12:47:34 +0300

    Add EntityNotFoundException.

---
+++ SchoolDiary/SchoolDiary.BLL/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.BLL.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(): base("Entity not found.")
+        {
+            
+        }
+
+        public EntityNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
