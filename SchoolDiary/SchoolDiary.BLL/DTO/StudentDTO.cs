b6a246652c45c8b3a67ae4af7b709d9effc4c119
akuklinov <akuklinov@outlook.com>
2024-08-24 13:22:42 +0300

    Add validation.

--- SchoolDiary/SchoolDiary.BLL/DTO/StudentDTO.cs
+++ SchoolDiary/SchoolDiary.BLL/DTO/StudentDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,20 @@ namespace SchoolDiary.BLL.DTO
     public class StudentDTO
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; } = null!;
+        [Required]
         public string LastName { get; set; } = null!;
+        [Required]
         public string MiddleName { get; set; } = null!;
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
+        [Required]
         public string PhoneNumber { get; set; } = null!;
+        [Required]
         public string Address { get; set; } = null!;
+        [Required]
         public DateTime Birthdate { get; set; }
         public DateTime EnrolnmentDate { get; set; }
         public int GroupId { get; set; }
--- SchoolDiary/SchoolDiary.WEB/Program.cs
+++ SchoolDiary/SchoolDiary.WEB/Program.cs
@@ -7,7 +7,7 @@ using SchoolDiary.DAL.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddAutoMapper(config =>
 {
     config.AddProfile<BusinessLogicLayerMappingProfile>();
